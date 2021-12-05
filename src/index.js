88b8e59d71e3d7776919ef234389443decd62262
ManzDev <JoseRomanHernandez@gmail.com>
2021-12-05 02:41:37 +0000

    Twitch (live)

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// const ovni = document.querySelector(".ovni");
+
+// const start = () => {
+//   ovni.classList.remove("stop");
+//   ovni.classList.toggle("on");
+// };
+
+// setTimeout(start, 4000);
